9fe7e148cee74ee3f7cf5a6e8c930b1ab52ec0e6
David Storey <david@lostincode.co.uk>
2015-05-25 20:39:24 +0100

    Testing Project Gutenberg sentiment

--- start.js
+++ start.js
@@ -1,25 +1,43 @@
-var twit      = require('twit');
 var sentiment = require('sentiment');
-var details   = require('./twitter_details')
-var idString  = '#got';
-var T         = new twit(details);
-var stream    = T.stream('statuses/filter', { track: idString });
 
-stream.on('connect', function(request) {
-	console.log('Connected to Twitter API');
-});
+// Test tweets
+function testTweets(idString) {
+	var twit      = require('twit');
+	var details   = require('./twitter_details')
+	var T         = new twit(details);
+	var stream    = T.stream('statuses/filter', { track: idString });
 
-stream.on('disconnect', function(message) {
-	console.log('Disconnected from Twitter API. Message: ' + message);
-});
+	stream.on('connect', function(request) {
+		console.log('Connected to Twitter API');
+	});
 
-stream.on('reconnect', function (request, response, connectInterval) {
-	console.log('Trying to reconnect to Twitter API in ' + connectInterval + ' ms');
-});
+	stream.on('disconnect', function(message) {
+		console.log('Disconnected from Twitter API. Message: ' + message);
+	});
 
-// Start listening for tweets
-stream.on('tweet', function(tweet) {
-	console.log(tweet.created_at + ' ' + tweet.user.name + ' says:  ' + tweet.text);
-	console.log(sentiment(tweet.text).score);
-	console.log('');
-});
+	stream.on('reconnect', function (request, response, connectInterval) {
+		console.log('Trying to reconnect to Twitter API in ' + connectInterval + ' ms');
+	});
+
+	// Start listening for tweets
+	stream.on('tweet', function(tweet) {
+		console.log(tweet.created_at + ' ' + tweet.user.name + ' says:  ' + tweet.text);
+		console.log(sentiment(tweet.text).score);
+		console.log('');
+	});
+}
+
+
+// Test books on Project Gutenberg
+function testGutenberg() {
+	var fs = require('fs');
+
+	var text = fs.readFileSync('david_copperfield.txt') + '';
+	console.log(sentiment(text).score);	
+}
+
+
+
+//testTweets('#got');
+
+testGutenberg();
--- twitter_details.js
+++ twitter_details.js
@@ -1,6 +1,6 @@
 module.exports = {
-	consumer_key:         '',
-	consumer_secret:      '',
-	access_token:         '',
-	access_token_secret:  ''
+	consumer_key:         'I7S9GeX2apfXoHBqLcvrV8lm6',
+	consumer_secret:      'Gc5s2LwymyITxw8mhGO3Rn4ykzVvMmzfeuROgsFqso2x2PWu67',
+	access_token:         '103855946-6Fa2hqK73Sl0wmqB6y69M0qkXmkVebnSeLPXItve',
+	access_token_secret:  'rwxh9NDETkH7yDPIeCR37hY9gtsp3wI03fYSwZhGGsNMe'
 };
